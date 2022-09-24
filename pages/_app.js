477ff036580d67e816ec90aa156a72a2627e9f8b
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-09-25 02:11:15 +0300

    vape

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { CartContext } from '../contexts/CartContext'
 import '../styles/globals.css'
 import '../styles/dadata.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 
@@ -10,9 +11,14 @@ function MyApp({ Component, pageProps }) {
 
 
   return (
+    <>
+    <Head>
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" /> */}
+    </Head>
     <CartContext.Provider value={[cartContext, setCartContext]}>
       <Component {...pageProps} />
     </CartContext.Provider>
+    </>
   )
 }
 
