67e04139cfc786a972c5f9e914d577a1f986e987
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-10-01 23:45:58 +0300

    fixes

--- pages/complete.js
+++ pages/complete.js
@@ -17,7 +17,7 @@ const Order = () => {
     <div className="container">
       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', height: '100vh'}}>
         <Icon width={100} height={100} color={'#503E8D'} icon="akar-icons:check-box" />
-        <h2 style={{margin: '10px 0px 20px', fontSize: 32}}>Ваш заказ успешно отправлен!</h2>
+        <h2 style={{margin: '10px 0px 20px', fontSize: 32, textAlign: 'center'}}>Ваш заказ успешно отправлен!</h2>
         <Link href="/"><button style={{padding: 10}} className="primary__button">На главную</button></Link>
       </div>
     </div>
