a98994e30af47e77018e8de4f63b24757c7f285c
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-10-01 21:13:08 +0300

    https

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
     </Head>
     <CategoryContext.Provider value={[categoryContext, setCategoryContext]}>
