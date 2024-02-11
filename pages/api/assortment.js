b4d5867c244d8635cde0a287f49820be1b9c97ce
Some <87341367+Ahmadteu@users.noreply.github.com>
2024-02-11 16:10:22 +0300

    delete sort by name

--- pages/api/assortment.js
+++ pages/api/assortment.js
@@ -1,18 +1,26 @@
 export default async function handler(req, res) {
-
-    const {
-        query: {category, search, offset},
-        method,
-      } = req
-    if (method === 'GET') {
-        let products = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/entity/assortment?expand=product,product.images,images&limit=100&offset=${offset}&filter=${search ? 'search=' + search + ';' : ''}stockMode=positiveOnly;stockStore=https://online.moysklad.ru/api/remap/1.2/entity/store/8179a7a1-c29d-11eb-0a80-048e00039ac0;quantityMode=positiveOnly${category ? ';pathname=' + category: ''}&order=name`, {
-            headers: {
-            'Authorization': process.env.NEXT_PUBLIC_DADATA_API_KEY
-            }
-        })
-        products = await products.json()
-        res.status(200).json(products)
-    } else {
-      // Handle any other HTTP method
-    }
+  const {
+    query: { category, search, offset },
+    method,
+  } = req;
+  if (method === "GET") {
+    let products = await fetch(
+      `${
+        process.env.NEXT_PUBLIC_API_HOST
+      }/entity/assortment?expand=product,product.images,images&limit=100&offset=${offset}&filter=${
+        search ? "search=" + search + ";" : ""
+      }stockMode=positiveOnly;stockStore=https://online.moysklad.ru/api/remap/1.2/entity/store/8179a7a1-c29d-11eb-0a80-048e00039ac0;quantityMode=positiveOnly${
+        category ? ";pathname=" + category : ""
+      }`,
+      {
+        headers: {
+          Authorization: process.env.NEXT_PUBLIC_DADATA_API_KEY,
+        },
+      }
+    );
+    products = await products.json();
+    res.status(200).json(products);
+  } else {
+    // Handle any other HTTP method
   }
+}
